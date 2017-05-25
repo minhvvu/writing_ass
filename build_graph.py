3e50260b751bb71fbbcb55c0063412bb9d7e9c7b
minhvvu <vn.vietvu@gmail.com>
2017-05-25 17:05:51 +0700

    Limit depth when searching path from one node to other node. Add function doc

--- build_graph.py
+++ build_graph.py
@@ -1,11 +1,16 @@
 # Build graph from pairs of word with its weight
-# Using negative weight for applying shortest-part algo
+# Produce function to get all paths from one node to other node
+# minhvvu 2017-05-25
 
 import networkx as nx
 
 import graph_utils as gutil
 
 def create_from_pairs(pairs):
+    """
+    Build graph from `pairs` of words.
+    Accumulate weight for the edges that appear multiple time
+    """
     DG = nx.DiGraph()
     for (n1, n2, w) in pairs:
         if DG.has_edge(n1, n2):
@@ -15,22 +20,35 @@ def create_from_pairs(pairs):
     return DG
 
 
-def normalize_weights(G):
+def inverse_weights(G):
+    """
+    Make the larger weight become smaller by taking its inverse
+    """
     for (n1, n2, w) in G.edges(data=True):
         G.edge[n1][n2]['weight'] = 1.0 / w['weight']
 
 
 def build(pairs, outname):
+    """
+    Build graph from `pairs` of word
+    and save the edges to text file `outname`
+    """
     G = create_from_pairs(pairs)
-    #normalize_weights(G)
+    #inverse_weights(G)
     gutil.write_edges(G, outname)
 
 
 def weighted_shortes_path(G, n1, n2):
+    """
+    Find the shortest path with weighted edges, using Dijkstra
+    """
     return nx.dijkstra_path(G, n1, n2)
 
 
 def sum_path(G, path):
+    """
+    Calculate sum of weight in each edges of `path`
+    """
     sum_weight = 0
     for i in range(len(path)-1):
         n1, n2 = path[i], path[i+1]
@@ -38,9 +56,13 @@ def sum_path(G, path):
     return sum_weight
 
 
-def all_paths(G, src, dest):
+def all_paths(G, src, dest, cutoff=4):
+    """
+    Find all paths from `src` to `dest` with default max depth in `cutoff`.
+    The result paths are sorted by their weight sum, larger comes first.
+    """
     return sorted([path
-        for path in nx.all_simple_paths(G, src, dest)],
+        for path in nx.all_simple_paths(G, src, dest, cutoff)],
          key=lambda path: -1 * sum_path(G, path))
 
 
@@ -66,8 +88,7 @@ def main():
     path = weighted_shortes_path(G, 'Nice', 'you')
     print(path)
 
-    print(all_paths(G, 'Nice', 'you'))
+    print(all_paths(G, 'Nice', 'you', cutoff=3))
 
-    
 if __name__ == '__main__':
     main()
