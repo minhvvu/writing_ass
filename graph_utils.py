fcc692bd156845a4a42866d574ed9215eaf750eb
minhvvu <vn.vietvu@gmail.com>
2017-05-25 17:12:49 +0700

    Add function doc to graph utils

--- graph_utils.py
+++ graph_utils.py
@@ -1,4 +1,6 @@
-# graph utils
+# Utils for print, read, write graph
+# Produce a simple version of draw function to visualize graph
+# minhvvu 2017-05-25
 
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -6,6 +8,10 @@ import sys
 
 
 def write_edges(H, outname=None, delimiter=';'):
+    """
+    Write list of edges to file `outname`.
+    If the file name is not passed, print to screen.
+    """
     if outname and outname != "":
         nx.write_edgelist(H, path=outname, delimiter=delimiter)
     else:
@@ -13,10 +19,18 @@ def write_edges(H, outname=None, delimiter=';'):
 
 
 def read_edges(inname, delimiter=';'):
+    """
+    Create a new graph by reading list of edges from file.
+    """
     return nx.read_edgelist(path=inname, delimiter=delimiter)
 
 
 def write_adj(H, outname=None):
+    """
+    Write adjacency list of each vertex to file `outname`.
+    If the file name is not passed, print to screen.
+
+    """
     if outname and outname != "":
         nx.write_adjlist(H,path= outname)
     else:
@@ -24,6 +38,9 @@ def write_adj(H, outname=None):
 
 
 def print_graph(H):
+    """
+    Print all edges of weighted and non-weighted graph.
+    """
     for edge in H.edges(data=True):
         if (len(edge) == 2):
             print(edge[0], ' -> ', edge[1])
